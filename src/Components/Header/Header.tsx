c0cc763c85bbdf8d0c104869ae3adc320a3f4a16
LoganW <logan.willaumez@gmail.com>
2022-07-28 16:45:15 +0200

    [FEAT](refactoring): change logo name

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
     <Html wrapperClass='header' as='div' prepend={true} zIndexRange={[100, 0]}>
       <div className='header__container'>
         <h1 className='header__title'>Customise your Sneakers !!</h1>
-        <img className='header__logo' src='/1200px-Swoosh.svg.png' alt='' />
+        <img className='header__logo' src='/nike-logo.png' alt='' />
       </div>
     </Html>
   );
